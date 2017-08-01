55da093ec08c8f0bebd8e8e29f2428c02f048383
gabrielbressani <g.bressaniribeiro@gmail.com>
2017-08-01 00:39:39 -0300

    Api Questions

---
+++ StarlineTest/StarlineTest.API/Controllers/QuestionsController.cs
@@ -0,0 +1,35 @@
+﻿using StarlineTest.Application.Interface;
+using StarlineTest.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StarlineTest.API.Controllers
+{
+    [Route("api/Questions")]
+    public class QuestionsController : ApiController
+    {
+        protected IQuestionAppService QuestionAppService { get; private set; }
+
+        public QuestionsController(IQuestionAppService questionAppService)
+        {
+            QuestionAppService = questionAppService;
+        }
+
+        // GET apli/Questions
+        public Question[] Get()
+        {
+            return QuestionAppService.GetAll().ToArray();
+        }
+
+        // GET apli/Questions/5
+        public string Get(int id)
+        {
+            return QuestionAppService.GetById(id).Answers;
+        }
+
+    }
+}
