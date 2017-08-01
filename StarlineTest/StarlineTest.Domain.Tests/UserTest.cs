779b2f01bd010f91026d1d2b34435f8ee700a9bd
gabrielbressani <g.bressaniribeiro@gmail.com>
2017-08-01 01:05:50 -0300

    Create Unit Tests

---
+++ StarlineTest/StarlineTest.Domain.Tests/UserTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StarlineTest.Domain.Entities;
+
+namespace StarlineTest.Domain.Tests
+{
+    [TestClass]
+    public class UserTest
+    {
+        [TestMethod]
+        public void If_User_Is_Active_And_Registration_Year_Date_Is_Actual_Year_Then_UserActiveOfThisYear_Is_True()
+        {
+            var user = new User()
+            {
+                Active = true,
+                RegistrationDate = DateTime.Now
+            };
+
+            var actual = user.UserActiveOfThisYear(user);
+
+            Assert.IsTrue(actual);
+        }
+    }
+}
